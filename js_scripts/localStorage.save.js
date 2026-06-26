b2ae5c85cb42e0068655a2d35bf68a9c883059e9
Eltaurus <eltaurus@inbox.lt>
2026-06-26 20:00:13 +0600

    js cleanup

--- js_scripts/localStorage.save.js
+++ js_scripts/localStorage.save.js
@@ -1,5 +1,5 @@
 (() => {
-    window.addEventListener('localStorageUPD', (e) => {
+    function localStorage_save() {
         if (window.__localStorage_isLoading) return;
         try {
             var localStorage_json = JSON.stringify(localStorage);
@@ -9,23 +9,23 @@
         } catch(err) {
             console.error("localStorage listener error: ", err);
         }
-    });
+    };
 
     var originalSetItem = localStorage.setItem;
     localStorage.setItem = function(key, value) {
         originalSetItem.apply(this, arguments);
-        window.dispatchEvent(new Event('localStorageUPD'));
+        localStorage_save();
     };
 
     var originalRemoveItem = localStorage.removeItem;
     localStorage.removeItem = function(key) {
         originalRemoveItem.apply(this, arguments);
-        window.dispatchEvent(new Event('localStorageUPD'));
+        localStorage_save();
     };
 
     var originalClear = localStorage.clear;
     localStorage.clear = function() {
         originalClear.apply(this, arguments);
-        window.dispatchEvent(new Event('localStorageUPD'));
+        localStorage_save();
     };
 })();
